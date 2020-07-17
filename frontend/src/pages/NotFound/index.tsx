c001c6c72ea8bdf0d30c3f5397c4209709c1c987
João Pedro Caires <joaopdante@gmail.com>
2020-07-17 09:46:00 -0300

    Remove NotFound img

--- frontend/src/pages/NotFound/index.tsx
+++ frontend/src/pages/NotFound/index.tsx
@@ -4,13 +4,6 @@ class NotFound extends React.Component {
   render() {
     return (
       <form className="form-signin">
-        <img
-          className="mb-4"
-          src="/docs/4.5/assets/brand/bootstrap-solid.svg"
-          alt=""
-          width="72"
-          height="72"
-        />
         <h1 className="h3 mb-3 font-weight-normal">404 - Page Not Found</h1>
       </form>
     );
